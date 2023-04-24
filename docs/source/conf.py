11f288f43bd3e9a130e02acf38bf08f4b6e4c3e6
pcurt <57074781+pcurt@users.noreply.github.com>
2023-04-24 10:09:30 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MAPIO'
+copyright = 'Pierrick Curt'
+author = 'Pierrick Curt'
 
 release = '0.1'
 version = '0.1.0'
